5f7bdcb31506f8c8678dff21f7d6b7cccef5bb53
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2023-01-20 02:48:43 +0545

    api img

--- server/routes/reportRoutes.js
+++ server/routes/reportRoutes.js
@@ -1,6 +1,8 @@
 import Express from "express";
 import mongoose from "mongoose";
 const router = Express.Router();
+import multer from "multer";
+
 import {
   postReport,
   getReportOfOne,
@@ -9,5 +11,33 @@ import {
 router.route("/postreport").post(postReport);
 router.route("/getreportofone").post(getReportOfOne);
 router.route("/getone").post(getOne);
+
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "." + file.originalname.split(".")[1]);
+  },
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+});
+
+const upload = multer({ storage: storage });
+
+//Img Url Route
+router.post("/web/upload", upload.single("pic"), async (req, res) => {
+  try {
+    if (req.file === undefined)
+      return res
+        .status(400)
+        .json({ error: false, msg: "You must select a file." });
+    const url =
+      req.protocol + "://" + req.get("host") + "/uploads/" + req.file.filename;
+    res
+      .status(200)
+      .json({ error: false, msg: "File uploaded successfully!", imgUrl: url });
+  } catch (error) {
+    res.status(400).json({ error: true, msg: "Img upload failed" });
+  }
+});
 
 export default router;
