7ce474d4edc5a3aaed79c8c6ce6b45c7b25fbb78
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2023-01-19 23:41:49 +0545

    ok

--- client/src/pages/Reports.jsx
+++ client/src/pages/Reports.jsx
@@ -3,34 +3,46 @@ import ReportComponent from "../components/ReportComponent";
 import styles from "../css/pages/Reports.module.css";
 import { BsSearch } from "react-icons/bs";
 
-
 const Reports = () => {
-  const column = [{
-    id:"sn",
-    label:"SN",
-    minWidth:50
-  },{
-    id:"department",
-    label:"Department",
-    minWidth:100
-  },
-  
-  {
-    id:"date",
-    label:"Date",
-    minWidth:100
-  },
-  {
-    id:"severity",
-    label:"Severity",
-    minWidth:100
-  },
-  {
-    id:"symptoms",
-    label:"Symptoms",
-    minWidth:100
-  }
-]
+  const getRecentReports = async () => {
+    console.log("okkkk");
+    const patientdata = await axiosinstance.post("/hospital/getrecentpatient", {
+      hm_hid: "12345",
+    });
+    await setData(patientdata.data.data);
+  };
+  const [data, setData] = React.useState([{}]);
+  getRecentReports();
+  console.log(data);
+
+  const column = [
+    {
+      id: "sn",
+      label: "SN",
+      minWidth: 50,
+    },
+    {
+      id: "department",
+      label: "Department",
+      minWidth: 100,
+    },
+
+    {
+      id: "date",
+      label: "Date",
+      minWidth: 100,
+    },
+    {
+      id: "severity",
+      label: "Severity",
+      minWidth: 100,
+    },
+    {
+      id: "symptoms",
+      label: "Symptoms",
+      minWidth: 100,
+    },
+  ];
   return (
     <div className={styles.reports_con}>
       <div className={styles.reports_top}>
@@ -43,7 +55,7 @@ const Reports = () => {
         </div>
       </div>
       <div className={styles.report_listing}>
-      <ReportComponent column={column}/>
+        <ReportComponent column={column} />
       </div>
     </div>
   );
