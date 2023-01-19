fe0a35651b219e603e8c4ff92799739f484ebb67
Sanskar <sskrlmsl@gmail.com>
2023-01-19 22:27:41 +0545

    Report display UI Component

--- client/src/pages/Patients.jsx
+++ client/src/pages/Patients.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {styles} from "../css/pages/Patients.module.css"
+import styles from "../css/pages/Patients.module.css"
 
 const Patients = () => {
   return (
     <>
     <div className={styles.patients_con}>
-
+      
     </div>
     </>
   )
