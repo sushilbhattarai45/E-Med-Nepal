08d989974c79e5d42c14ec6e374dc229735a90f8
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2023-01-20 03:44:31 +0545

    current prescription

--- server/controllers/reportController.js
+++ server/controllers/reportController.js
@@ -76,7 +76,7 @@ export const prescription = async (req, res) => {
     let prescription = [];
     data.map((item) => {
       var result = new Date(item?.r_toc?.date);
-      const day = item?.r_prescription[0]?.duration;
+      const day = item.r_prescription[0]?.duration;
       console.log(day);
       result.setDate(result.getDate() + day);
       const durationdate = result.toISOString().split("T")[0];
