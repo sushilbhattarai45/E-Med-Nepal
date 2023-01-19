8445954d46594ce43c5ef45d0544bf6cd6467178
Utsav bhattarai <95236817+utsavbhattarai007@users.noreply.github.com>
2023-01-20 02:47:00 +0545

    form

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -43,7 +43,11 @@ export default function Router() {
         {
             path:"report/:id",
             element:<Report/>
+        },
+        {
+          path:"reportpost/:id"
         }
+
       ],
     },
   ]);
