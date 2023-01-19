446b75dd8b4755b75397060e2478b5b8f9dc4321
Sanskar <sskrlmsl@gmail.com>
2023-01-20 01:19:50 +0545

    Report screen updated

--- client/src/pages/Report.jsx
+++ client/src/pages/Report.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "../css/pages/Report.module.css";
 import { BiArrowBack } from "react-icons/bi";
 
+const symptoms = [1, 2, 3];
+
 const Report = () => {
   return (
     <>
@@ -20,19 +22,22 @@ const Report = () => {
                   <p className={styles.title}>Patients Name</p>
                   <p className={styles.value}>John Doe</p>
                 </div>
-                <div className={styles.top_right}>
-                  <p className={styles.depart_name}>Kathmandu</p>
-                </div>
+                <div className={styles.top_right}>Kathmandu</div>
               </div>
               <div className={styles.mid}>
                 <span className={styles.title}>Description</span>
                 <span className={styles.des}>
-                  hello hi ta tatjsaakdhdakdjhsjd sgdnsd dsh h 
-                  hello hi ta tatjsaakdhdakdjhsjd sgdnsd dsh h 
+                  hello hi ta tatjsaakdhdakdjhsjd sgdnsd dsh h hello hi ta
+                  tatjsaakdhdakdjhsjd sgdnsd dsh h
                 </span>
               </div>
               <div className={styles.bottom}>
                 <div className={styles.title}>Symptoms</div>
+                <div className={styles.symptoms}>
+                  {symptoms.map(() => (
+                    <div className={styles.symptom_con}>Fever</div>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
